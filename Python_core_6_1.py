a2b62d850e1ca48dc351ff72398af7dc8f1e909b
Mari86957 <133572710+Mari86957@users.noreply.github.com>
2024-08-17 13:25:47 +0300

    Add files via upload

--- Python_core_6_1.py
+++ Python_core_6_1.py
@@ -1,83 +1,4 @@
 
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
-
-def input_error(add_contact):
-    def inner(*args, **kwargs):
-        try:
-            return add_contact(*args, **kwargs)
-        except ValueError:
-            return "Give me name and phone please."
-        except KeyError:
-            return "This contact does not exist."
-        except IndexError:
-            return "Enter user name."
-    return inner
-
-@input_error
-def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
-
-@input_error
-def change_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact changed"
-
-@input_error
-def phone_username(args, contacts):
-    name = args[0]
-    return contacts[name]
-
-
-def show_all(contacts):
-    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
-
-def main():
-    contacts = {}
-    print("Welcome to the assistant bot!")
-    while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
-
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print (change_contact(args, contacts))
-        elif command == "phone":
-            print(phone_username(args, contacts))
-        elif command == "all":
-            print(show_all(contacts))
-        else:
-            print("Invalid command.")
-
-if __name__ == "__main__":
-    main()
-
-
-def input_error(add_contact):
-    def inner(*args, **kwargs):
-        try:
-            return add_contact(*args, **kwargs)
-        except ValueError:
-            return "Give me name and phone please."
-        except KeyError:
-            return "This contact does not exist."
-        except IndexError:
-            return "Enter user name."
-    return inner
-
-
-
 from collections import UserDict
 
 class Field:
@@ -88,7 +9,6 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-		# self.name = name
     pass
 
 class Phone(Field):
@@ -111,11 +31,12 @@ class Record:
         if phone_to_remove:
              self.phones.remove(phone_number)
 
-    def edit_phone(self, old_number, new_number): 
+    def edit_phone(self, old_number, new_number):
         phone_to_edit = self.find_phone(old_number)
         if phone_to_edit:
-             self.phones.remome(old_number)
-             self.phones.append(new_number)
+            self.phones.remove(phone_to_edit)
+            self.add_phone(new_number)
+
 
     def find_phone(self, phone_number):
          for phone in self.phones:
